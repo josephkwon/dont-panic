69b6de3e1dd8990a731b506bf55effb82e5b1958
Josh <jpsteffe@jpsteffe-laptop.(none)>
2015-07-18 18:58:01 -0500

    Added settings fragment layout

--- mobile/src/main/java/coffee/chris/gopherstudios/dontpanic/settings.java
+++ mobile/src/main/java/coffee/chris/gopherstudios/dontpanic/settings.java
@@ -5,6 +5,8 @@ import android.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 
@@ -12,8 +14,17 @@ public class settings extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState){
         View view = inflater.inflate(R.layout.fragment_settings, container, false);
-        TextView textview = (TextView) view.findViewById(R.id.tabtextview);
-        textview.setText(R.string.TWO);
+
+        EditText nameField = (EditText)view.findViewById(R.id.Name);
+        EditText phoneField = (EditText)view.findViewById(R.id.Phone);
+        Button saveContactButton = (Button)view.findViewById(R.id.saveContact);
+
+        EditText messageField = (EditText)view.findViewById(R.id.Message);
+        Button saveMessageButton = (Button)view.findViewById(R.id.saveMessage);
+
         return view;
+    }
+
+    public void saveContact(View v){
     }
 }
