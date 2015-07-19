35324352c187fa088dabae49b0e4bb770fe84114
joseph <josephkwon@wustl.edu>
2015-07-18 22:00:59 -0500

    working version with sending int

--- mobile/src/main/java/coffee/chris/gopherstudios/dontpanic/main.java
+++ mobile/src/main/java/coffee/chris/gopherstudios/dontpanic/main.java
@@ -4,14 +4,42 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.PendingResult;
+import com.google.android.gms.wearable.DataApi;
+import com.google.android.gms.wearable.DataEventBuffer;
+import com.google.android.gms.wearable.DataItem;
+import com.google.android.gms.wearable.DataEvent;
+import com.google.android.gms.wearable.DataMap;
+import com.google.android.gms.wearable.DataMapItem;
+import com.google.android.gms.wearable.PutDataRequest;
+import com.google.android.gms.wearable.Wearable;
+import com.google.android.gms.wearable.PutDataMapRequest;
 
 
-public class main extends Activity {
+public class main extends Activity implements
+        DataApi.DataListener,
+        GoogleApiClient.ConnectionCallbacks,
+        GoogleApiClient.OnConnectionFailedListener {
+
+    private String EVENTS_PANIC = "/Events/PanicFired";
+    private String PANIC_KEY = "coffee.chris.gopherstudios.panic";
+    private GoogleApiClient mGoogleApiClient;
+    private long panicTime = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addApi(Wearable.API)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .build();
+        ((TextView)findViewById(R.id.mTextView)).setText(""+panicTime);
     }
 
     @Override
@@ -34,5 +62,59 @@ public class main extends Activity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onStart();
+        mGoogleApiClient.connect();
+    }
+
+    @Override
+    public void onConnected(Bundle b) {
+        Wearable.DataApi.addListener(mGoogleApiClient, this);
+        PutDataMapRequest pdmr = PutDataMapRequest.create(EVENTS_PANIC);
+        PutDataRequest pdr = pdmr.asPutDataRequest();
+        PendingResult<DataApi.DataItemResult> pr = Wearable.DataApi.putDataItem(mGoogleApiClient,pdr);
+        panicTime = pdmr.getDataMap().getLong(PANIC_KEY);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Wearable.DataApi.removeListener(mGoogleApiClient, this);
+        mGoogleApiClient.disconnect();
+    }
+
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    public void onConnectionFailed(ConnectionResult r) {
+
+    }
+
+    public void onDataChanged(DataEventBuffer d) {
+        for (DataEvent event : d) {
+            if (event.getType() == DataEvent.TYPE_CHANGED) {
+                // DataItem changed
+                DataItem item = event.getDataItem();
+                if (item.getUri().getPath().compareTo(EVENTS_PANIC) == 0) {
+                    DataMap dataMap = DataMapItem.fromDataItem(item).getDataMap();
+                    updateStatus(dataMap.getLong(PANIC_KEY));
+                }
+            } else if (event.getType() == DataEvent.TYPE_DELETED) {
+                // DataItem deleted
+            }
+        }
+    }
+
+    private void updateStatus(final long i) {
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                ((TextView) findViewById(R.id.mTextView)).setText("" + i);
+            }
+        });
     }
 }
--- wear/src/main/java/coffee/chris/gopherstudios/dontpanic/main.java
+++ wear/src/main/java/coffee/chris/gopherstudios/dontpanic/main.java
@@ -2,31 +2,123 @@ package coffee.chris.gopherstudios.dontpanic;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.support.wearable.view.WatchViewStub;
-import android.widget.TextView;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 
-public class main extends Activity {
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.PendingResult;
+import com.google.android.gms.wearable.DataApi;
+import com.google.android.gms.wearable.PutDataRequest;
+import com.google.android.gms.wearable.Wearable;
+import com.google.android.gms.wearable.PutDataMapRequest;
 
-    private TextView mTextView;
+public class main extends Activity implements
+        GoogleApiClient.ConnectionCallbacks,
+        GoogleApiClient.OnConnectionFailedListener {
+
+    private String EVENTS_PANIC = "/Events/PanicFired";
+    private String PANIC_KEY = "coffee.chris.gopherstudios.panic";
+    private GoogleApiClient mGoogleApiClient;
+    private long panicTime, startTime = 0;
+    private boolean held = false;
+    private View.OnTouchListener toucher;
+    private Thread uiUpdateThread;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        final WatchViewStub stub = (WatchViewStub) findViewById(R.id.watch_view_stub);
-        stub.setOnLayoutInflatedListener(new WatchViewStub.OnLayoutInflatedListener() {
-            @Override
-            public void onLayoutInflated(WatchViewStub stub) {
-                mTextView = (TextView) stub.findViewById(R.id.text);
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addApi(Wearable.API)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .build();
+
+        uiUpdateThread = new Thread(new Runnable(){
+            public void run()
+            {
+                while(true) {
+                    if (held) {
+                        panicHeld();
+                    }
+                }
             }
         });
+
+        uiUpdateThread.start();
+
+                PutDataMapRequest putDataMapReq = PutDataMapRequest.create(EVENTS_PANIC);
+        panicTime = putDataMapReq.getDataMap().getLong(PANIC_KEY);
+        ((Button)findViewById(R.id.shitButton)).setText("" + panicTime);
+
+        toucher = new View.OnTouchListener(){
+
+            public boolean onTouch(View v, MotionEvent event)
+            {
+                if(event.getAction() == MotionEvent.ACTION_DOWN)
+                {
+                    //Start ticker
+                    held = true;
+                    startTime = System.currentTimeMillis();
+                    return true;
+                }
+                else if (event.getAction() == MotionEvent.ACTION_UP) {
+                    // Stop ticker
+                    held = false;
+                    panicTime = 0;
+                    PutDataMapRequest putDataMapReq = PutDataMapRequest.create(EVENTS_PANIC);
+                    putDataMapReq.getDataMap().putLong(PANIC_KEY, panicTime);
+                    PutDataRequest putDataReq = putDataMapReq.asPutDataRequest();
+                    PendingResult<DataApi.DataItemResult> pendingResult =
+                            Wearable.DataApi.putDataItem(mGoogleApiClient, putDataReq);
+                    return true;
+                }
+
+                return false;
+            }
+        };
+
+        findViewById(R.id.shitButton).setOnTouchListener(toucher);
     }
 
-    private void clickSHIT(View view)
-    {
-        final Button b = (Button)findViewById(R.id.shitButton);
-        b.setText("Clicked");
+
+    @Override
+    protected void onResume() {
+        super.onStart();
+        mGoogleApiClient.connect();
     }
+
+    @Override
+    public void onConnected(Bundle b) {
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        held = false;
+        mGoogleApiClient.disconnect();
+    }
+
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    public void onConnectionFailed(ConnectionResult r) {
+
+    }
+
+    private void panicHeld()
+    {
+        panicTime = System.currentTimeMillis() - startTime;
+        PutDataMapRequest putDataMapReq = PutDataMapRequest.create(EVENTS_PANIC);
+        putDataMapReq.getDataMap().putLong(PANIC_KEY, panicTime);
+        PutDataRequest putDataReq = putDataMapReq.asPutDataRequest();
+        PendingResult<DataApi.DataItemResult> pendingResult =
+                Wearable.DataApi.putDataItem(mGoogleApiClient, putDataReq);
+        ((Button)findViewById(R.id.shitButton)).setText("" + panicTime);
+    }
+
 }
